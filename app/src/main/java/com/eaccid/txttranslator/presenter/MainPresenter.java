abe21eb4272ddf3354daf3922b9e40368839fa11
EAccid <elena.briginets@gmail.com>
2017-01-05 17:23:51 +0200

    add main_screen img

--- app/src/main/java/com/eaccid/txttranslator/presenter/MainPresenter.java
+++ app/src/main/java/com/eaccid/txttranslator/presenter/MainPresenter.java
@@ -2,6 +2,11 @@ package com.eaccid.txttranslator.presenter;
 
 import com.eaccid.txttranslator.provider.fromtext.WordFromText;
 import com.eaccid.txttranslator.provider.translator.WordDataProvider;
+
+import java.net.UnknownHostException;
+import java.util.List;
+
+import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 
@@ -25,13 +30,27 @@ public class MainPresenter implements BasePresenter<MainActivity> {
     }
 
     public void onWordClicked(WordFromText wordFromText) {
-        mView.showToast(wordFromText.getText() + "has been selected");
-//        List<String> translations = dataProvider.procureTranslations(wordFromText);
-//        mView.showTextTranslation(translations);
+        mView.showToast(wordFromText.getText() + " has been selected");
         dataProvider.procureTranslationsObservable(wordFromText)
                 .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread()).subscribe(translations -> {
-            mView.showTextTranslation(translations);
+                .observeOn(AndroidSchedulers.mainThread()).subscribe(new Subscriber<List<String>>() {
+            @Override
+            public void onCompleted() {
+                unsubscribe();
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                e.printStackTrace();
+                if (e instanceof UnknownHostException) {
+                    mView.showToast("Check internet connection");
+                }
+            }
+
+            @Override
+            public void onNext(List<String> translations) {
+                mView.showTextTranslation(translations);
+            }
         });
     }
 
