5940b018b6b500d496695a2ef1082d49421b3a0b
EAccid <elena.briginets@gmail.com>
2017-01-07 19:06:02 +0200

    add okcache-control library. cache still doesn't work correctly

--- app/src/main/java/com/eaccid/txttranslator/injection/module/HttpModule.java
+++ app/src/main/java/com/eaccid/txttranslator/injection/module/HttpModule.java
@@ -1,18 +1,17 @@
 package com.eaccid.txttranslator.injection.module;
 
 import android.app.Application;
-
 import com.eaccid.txttranslator.presenter.NetworkAvailablenessImpl;
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.ncornette.cache.OkCacheControl;
+import java.util.concurrent.TimeUnit;
 import javax.inject.Singleton;
 import dagger.Module;
 import dagger.Provides;
 import okhttp3.Cache;
 import okhttp3.OkHttpClient;
-import okhttp3.Request;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -40,28 +39,18 @@ public class HttpModule {
         return gsonBuilder.create();
     }
 
-    /**
-     * /todo: handle cache
-     * isNetworkAvailable() -> true: Interceptor to cache data and maintain it for four weeks.
-     * isNetworkAvailable() -> false: Interceptor to cache data and maintain it for a minute.
-     */
     @Provides
     @Singleton
     OkHttpClient provideOkHttpClient(Cache cache) {
-        return new OkHttpClient
-                .Builder()
-                .cache(cache)
-                .addInterceptor(chain -> {
-                    Request request = chain.request();
-                    if (new NetworkAvailablenessImpl().isNetworkAvailable()) {
-                        request = request.newBuilder().header("Cache-Control", "public, max-age=" + 60).build();
-                    } else {
-                        //TODO doesn't work correctly: return "HTTP 504 Unsatisfiable Request (only-if-cached)"
-                        request = request.newBuilder().header("Cache-Control", "public, only-if-cached, max-stale=" + 60 * 60 * 24 * 28).build();
-                    }
-                    return chain.proceed(request);
-                })
-                .build();
+        final OkCacheControl.NetworkMonitor networkMonitor = provideNetworkMonitor();
+        final long timeValue = 30;
+        final TimeUnit timeUnit = TimeUnit.SECONDS;
+        return OkCacheControl
+                .on(new OkHttpClient.Builder())
+                .forceCacheWhenOffline(networkMonitor)
+                .overrideServerCachePolicy(timeValue, timeUnit)
+                .apply()
+                .cache(cache).build();
     }
 
     @Provides
